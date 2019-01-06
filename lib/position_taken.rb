e8a7a54c6f5d98cd76a1152b51338f226ce8c95a
Phan Huynh Tan <tanb1509892@student.ctu.edu.vn>
2019-01-06 14:37:43 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
-def position_taken?(board, pos = 0)
+def position_taken?(board = Array.new(9, " "), pos = 0)
   if "0X".include?(board[pos])
     return true
   end
